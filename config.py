d9db4ea7580ec24319b500deae7dd22184f2997c
Sean <seanxu1015@gmail.com>
2017-04-24 20:25:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ class Config:
     dev_path   = './data/dev-v1.1.json'
     save = './save'
     max_ctx_len = 600
-    hid_size = 200
+    hid_size = 100
     emb_size = 300
-    max_iter = 4
-    maxout_pool_size = 16
+    max_iter = 2
+    maxout_pool_size = 4
     epochs = 10
-    batch_size = 50
+    batch_size = 100
     learning_rate = 1e-3
     weight_decay = 1e-5
     device = '0'
